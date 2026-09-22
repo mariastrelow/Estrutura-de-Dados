07759b1ee28cbf0d70e5a798561bfd8270740d3c
Guilherme Arcas <144063470+GuilhermeArcas@users.noreply.github.com>
2026-09-22 14:31:38 -0400

    Revert "Adiciona pagamento a prazo com juros"

--- estoque.h
+++ estoque.h
@@ -1,31 +1,18 @@
 #ifndef ESTOQUE_H
-
 #define ESTOQUE_H
 
 #define MAX_ITENS 10
-
 #define TAXA_PADRAO 0.05
 
-#define TAXA_JUROS 0.08
-
 typedef struct {
-
     int id;
-
     char nome[30];
-
     float preco;
-
     int quantidade;
-
 } Produto;
 
 void exibir_menu(void);
-
 void listar_produtos(Produto lista[], int total);
-
 float calcular_total(Produto lista[], int total);
-
-float aplicar_juros(float total);
 
 #endif
